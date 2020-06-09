b9ab84bfc6f2cae3bb61e5ccf88cd317bdbb3cf9
Jorge Pietra Santa <a00226522@itesm.mx>
2020-06-09 01:59:35 -0700

    Added image taking and uploading for profile

--- app/src/main/java/com/example/android/foodtracker/RegistrationActivity.java
+++ app/src/main/java/com/example/android/foodtracker/RegistrationActivity.java
@@ -1,29 +1,47 @@
 package com.example.android.foodtracker;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText emailTV, passwordTV;
+    private EditText emailTV, passwordTV, nameTV, ageTV;
     private Button regBtn;
     private ProgressBar progressBar;
+    private FloatingActionButton imageBtn;
+    private ImageView profileImage;
 
     private FirebaseAuth mAuth;
+
+    private static final int MY_CAMERA_PERMISSION_CODE = 100, MY_READ_WRITE_PERMISSION_CODE = 101;
 
 
     @Override
@@ -41,14 +59,23 @@ public class RegistrationActivity extends AppCompatActivity {
                 registerNewUser();
             }
         });
+        imageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setImage(RegistrationActivity.this);
+            }
+        });
     }
 
     private void registerNewUser() {
         progressBar.setVisibility(View.VISIBLE);
 
-        String email, password;
+        String email, password, name, ageTxt;
         email = emailTV.getText().toString();
         password = passwordTV.getText().toString();
+        name = nameTV.getText().toString();
+        ageTxt = ageTV.getText().toString();
+        int age = 0;
 
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(getApplicationContext(), "Enter email", Toast.LENGTH_SHORT).show();
@@ -58,6 +85,16 @@ public class RegistrationActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_LONG).show();
             return;
         }
+        if (TextUtils.isEmpty(name)) {
+            Toast.makeText(getApplicationContext(), "Enter name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(ageTxt)) {
+            Toast.makeText(getApplicationContext(), "Enter age", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        age = Integer.parseInt(ageTxt);
 
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -81,7 +118,82 @@ public class RegistrationActivity extends AppCompatActivity {
     private void initializeUI() {
         emailTV = findViewById(R.id.registerEmail);
         passwordTV = findViewById(R.id.registerPassword);
+        nameTV = findViewById(R.id.registerName);
+        ageTV = findViewById(R.id.registerAge);
         regBtn = findViewById(R.id.register);
         progressBar = findViewById(R.id.registerProgressBar);
+        profileImage = findViewById(R.id.profileImage);
+        imageBtn = findViewById(R.id.imageBtn);
+    }
+
+    private void setImage(Context context) {
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Choose your profile picture");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Take Photo")) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                    {
+                        requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                    }
+                    else {
+                        Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(takePicture, 0);
+                    }
+
+                } else if (options[item].equals("Choose from Gallery")) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_READ_WRITE_PERMISSION_CODE);
+                    } else {
+                        Intent pickPhoto = new Intent(Intent.ACTION_PICK);
+                        pickPhoto.setType("image/*");
+                        startActivityForResult(pickPhoto, 1);
+                    }
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bundle extras = data.getExtras();
+                        Bitmap selectedImage = (Bitmap) extras.get("data");
+                        profileImage.setImageBitmap(selectedImage);
+                    }
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage,
+                                    filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                profileImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }
+
+                    }
+                    break;
+            }
+        }
     }
 }
